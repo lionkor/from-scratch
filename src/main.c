03d0f0b4c07b04f14c59df54cec22688e574641c
Lion Kortlepel <development@kortlepel.com>
2021-03-14 14:04:42 +0100

    main.c: reorganize

--- src/main.c
+++ src/main.c
@@ -1,20 +1,18 @@
 #include <X11/X.h>
 #include <X11/Xlib.h>
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glx.h>
 
 #include "err.h"
+#include "mem.h"
 
 enum {
-    RECT_X = 20,
-    RECT_Y = 20,
-    RECT_W = 10,
-    RECT_H = 10,
-
     WIN_X = 0,
     WIN_Y = 0,
     WIN_W = 600,
@@ -23,13 +21,13 @@ enum {
 };
 
 void DrawAQuad() {
+    log("redraw!");
     glClearColor(1.0, 1.0, 1.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glOrtho(-1., 1., -1., 1., 1., 20.);
-
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     gluLookAt(0., 0., 10., 0., 0., 0., 0., 1., 0.);
@@ -48,87 +46,119 @@ void DrawAQuad() {
     glEnd();
 }
 
+typedef struct {
+    int screen;
+    Display* display;
+    Window root;
+    GLint* att;
+    XVisualInfo* vi;
+    Colormap cmap;
+    XSetWindowAttributes swa;
+    Window window;
+    XWindowAttributes gwa;
+    GLXContext glc;
+} XGLEnvironment;
+
+static void main_loop(XGLEnvironment* env) {
+    assert(env);
+    XEvent event;
+    // event loop
+    bool shutdown = false;
+    while (!shutdown) {
+        if (XPending(env->display)) {
+            XNextEvent(env->display, &event);
+            switch (event.type) {
+
+            case ClientMessage:
+                shutdown = true;
+                break;
+            case KeyPress:
+                // fallthrough
+            case Expose: {
+                XGetWindowAttributes(env->display, env->window, &env->gwa);
+                glViewport(0, 0, env->gwa.width, env->gwa.height);
+                DrawAQuad();
+                glXSwapBuffers(env->display, env->window);
+                break;
+            }
+            }
+        }
+        XGetWindowAttributes(env->display, env->window, &env->gwa);
+        glViewport(0, 0, env->gwa.width, env->gwa.height);
+        DrawAQuad();
+        glXSwapBuffers(env->display, env->window);
+    }
+}
+
+static void init(XGLEnvironment* env) {
+    assert(env);
+    env->display = XOpenDisplay(NULL);
+    if (!env->display) {
+        log("X: cannot open display");
+        exit(1);
+    }
+    env->screen = DefaultScreen(env->display);
+    env->root = XDefaultRootWindow(env->display);
+    env->att = (GLint[]) { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };
+    env->vi = glXChooseVisual(env->display, 0, env->att);
+    if (!env->vi) {
+        log("glX: no visual found");
+        exit(1);
+    }
+    env->cmap = XCreateColormap(env->display, env->root, env->vi->visual, AllocNone);
+    env->swa.colormap = env->cmap;
+    env->swa.event_mask = ExposureMask | KeyPressMask;
+    env->window = XCreateWindow(env->display, env->root,
+        WIN_X, WIN_Y, WIN_W, WIN_H, WIN_BORDER,
+        env->vi->depth, InputOutput, env->vi->visual,
+        CWColormap | CWEventMask, &env->swa);
+
+    log("x properties:");
+    log("\tscreen dim (px)   : %dx%d", XDisplayWidth(env->display, env->screen), XDisplayHeight(env->display, env->screen));
+    log("\tscreen dim (mm)   : %dmm x %dmm", XDisplayWidthMM(env->display, env->screen), XDisplayHeightMM(env->display, env->screen));
+    log("\tdepth             : %d", XDefaultDepth(env->display, env->screen));
+    log("\tdisplay           : %s", XDisplayString(env->display));
+    log("\tprotocol version  : %d.%d", XProtocolVersion(env->display), XProtocolRevision(env->display));
+    log("\tserver vendor     : %s", XServerVendor(env->display));
+    log("gl properties:");
+    log("\tselected visual   : %p", (void*)env->vi->visualid);
+
+    // process window close event through event handler
+    Atom del_window = XInternAtom(env->display, "WM_DELETE_WINDOW", 0);
+    XSetWMProtocols(env->display, env->window, &del_window, 1);
+
+    // select which kinds of events we're interested in
+    XSelectInput(env->display, env->window, ExposureMask | KeyPressMask);
+
+    // display the window
+    XMapWindow(env->display, env->window);
+    XStoreName(env->display, env->window, "from_scratch");
+
+    env->glc = glXCreateContext(env->display, env->vi, NULL, GL_TRUE);
+    glXMakeCurrent(env->display, env->window, env->glc);
+    glEnable(GL_DEPTH_TEST);
+}
+
+static void deinit(XGLEnvironment* env) {
+    glXMakeCurrent(env->display, None, NULL);
+    glXDestroyContext(env->display, env->glc);
+    XDestroyWindow(env->display, env->window);
+    XCloseDisplay(env->display);
+}
+
 int main(int argc, char** argv) {
     if (argc != 1) {
         log("%s takes no arguments.\n", argv[0]);
         return 1;
     }
-    Display* display = XOpenDisplay(NULL);
-    if (!display) {
-        log("X: cannot open display");
-        return 1;
-    }
-    int screen = DefaultScreen(display);
-    Window root = XDefaultRootWindow(display);
-    GLint att[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };
-    XVisualInfo* vi = glXChooseVisual(display, 0, att);
-    if (!vi) {
-        log("glX: no visual found");
-        return 1;
-    }
-    Colormap cmap = XCreateColormap(display, root, vi->visual, AllocNone);
-    XSetWindowAttributes swa;
-    swa.colormap = cmap;
-    swa.event_mask = ExposureMask | KeyPressMask;
-    Window window = XCreateWindow(display, root,
-        WIN_X, WIN_Y, WIN_W, WIN_H, WIN_BORDER,
-        vi->depth, InputOutput, vi->visual,
-        CWColormap | CWEventMask, &swa);
 
-    log("x properties:");
-    log("\tscreen dim (px)   : %dx%d", XDisplayWidth(display, screen), XDisplayHeight(display, screen));
-    log("\tscreen dim (mm)   : %dmm x %dmm", XDisplayWidthMM(display, screen), XDisplayHeightMM(display, screen));
-    log("\tdepth             : %d", XDefaultDepth(display, screen));
-    log("\tdisplay           : %s", XDisplayString(display));
-    log("\tprotocol version  : %d.%d", XProtocolVersion(display), XProtocolRevision(display));
-    log("\tserver vendor     : %s", XServerVendor(display));
-    log("gl properties:");
-    log("\tselected visual   : %p", (void*)vi->visualid);
-
-    // process window close event through event handler
-    Atom del_window = XInternAtom(display, "WM_DELETE_WINDOW", 0);
-    XSetWMProtocols(display, window, &del_window, 1);
-
-    // select which kinds of events we're interested in
-    XSelectInput(display, window, ExposureMask | KeyPressMask);
-
-    // display the window
-    XMapWindow(display, window);
-    XStoreName(display, window, argv[0]);
-
-    GLXContext glc = glXCreateContext(display, vi, NULL, GL_TRUE);
-    glXMakeCurrent(display, window, glc);
-    glEnable(GL_DEPTH_TEST);
-
-    XEvent event;
-    // event loop
-    while (true) {
-        XNextEvent(display, &event);
-        switch (event.type) {
-        case KeyPress:
-            // fallthrough
-        case ClientMessage:
-            goto breakout;
-        case Expose: {
-            XWindowAttributes gwa;
-            XGetWindowAttributes(display, window, &gwa);
-            glViewport(0, 0, gwa.width, gwa.height);
-            DrawAQuad();
-            glXSwapBuffers(display, window);
-            /*
-            // draw the window
-            XFillRectangle(display, window, DefaultGC(display, screen),
-                RECT_X, RECT_Y, RECT_W, RECT_H);
-                */
-            break;
-        }
-        }
-    }
-breakout:
+    XGLEnvironment* env = allocate(sizeof(XGLEnvironment));
+    log("initializing...");
+    init(env);
+    log("main loop...");
+    main_loop(env);
     log("closing normally...");
-    glXMakeCurrent(display, None, NULL);
-    glXDestroyContext(display, glc);
-    XDestroyWindow(display, window);
-    XCloseDisplay(display);
+    deinit(env);
+    deallocate((void**)&env);
     return 0;
 }
